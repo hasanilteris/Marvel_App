4332c852d00f8945193343299f56a7b461f2ba27
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2022-03-16 23:17:53 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+
+
+//const hash= "03247d10af559a64150bce620ec1f01e"
+
+//(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f929fc77911a47c2a28a5fda5cb17dcb&hash=${hash}`)
+import './App.css';
+import Header from './components/Header'
+import CharacterTable from './components/CharacterTable'
+import axios from 'axios'
+import React , {useEffect,useState} from 'react'
+import Search from './components/Search'
+
+const hash =  "03247d10af559a64150bce620ec1f01e"
+
+function App() {
+  const[items,setItems] = useState([])
+  const[isLoading,setLoading] = useState(true)
+  const [query,setQuery] = useState('')
+
+  useEffect(()=>{
+      const fetch = async()=>{
+        if(query===''){
+          if(localStorage.getItem('favorites')==='[]' || !localStorage.getItem('favorites')){
+            localStorage.setItem('favorites', '[]')
+            const result = await axios(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f929fc77911a47c2a28a5fda5cb17dcb&hash=${hash}`)
+            console.log(result.data.data.results)
+            setItems(result.data.data.results)
+            setLoading(false) 
+          }else{
+            let favorite = JSON.parse(localStorage.getItem('favorites'))
+            setItems(favorite)
+            setLoading(false)
+          }
+          
+          
+        }else{
+          const result = await axios(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&ts=1&apikey=f929fc77911a47c2a28a5fda5cb17dcb&hash=${hash}`)
+          console.log(result.data.data.results)
+          setItems(result.data.data.results)
+          setLoading(false)
+        }
+      
+    }
+
+    fetch()
+  },[query])
+
+  return (
+    <div className="container">
+      <Header />
+      <Search search={(q)=>setQuery(q)}></Search>
+      <CharacterTable items={items} isLoading={isLoading} />
+    </div>
+  );
+}
+
+export default App;
